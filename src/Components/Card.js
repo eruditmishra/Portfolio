15672162f447a3b0b07650506797ae90846f1824
eruditmishra <eruditmishra@gmail.com>
2023-06-02 00:53:02 +0530

    fix - added rel=noreferrer

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ project }) => {
   return (
     <>
       <div className=" shadow-card rounded-2xl overflow-hidden">
-        <a href={project.link} target="_blank">
+        <a href={project.link} target="_blank" rel="noreferrer">
           <img
             src={project.image}
             alt=""
@@ -31,13 +31,13 @@ const Card = ({ project }) => {
           </span>
         </div>
         <div className="flex gap-4 mt-4 justify-center text-[1.08rem] leading-[1.5rem] font-medium text-[#2D2E32]">
-          <a href={project.code} target="_blank">
+          <a href={project.code} target="_blank" rel="noreferrer">
             <button className="flex gap-2 items-center">
               <p>Code</p>
               <AiFillGithub className="text-[2rem]" />
             </button>
           </a>
-          <a href={project.link} target="_blank">
+          <a href={project.link} target="_blank" rel="noreferrer">
             <button className="flex gap-2 items-center">
               <p>Live Demo </p>
               <img src={ArrowIcon} alt="" className="w-[1.5rem]" />
