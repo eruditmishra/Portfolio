99de9f66e0f728337d1152b221bde97ba1bd2988
eruditmishra <eruditmishra@gmail.com>
2023-07-23 23:48:59 +0530

    Fix - Footer text position fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,36 +3,33 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    // Footer Wrapper
-    <div className="bg-[#2d2e32] w-full">
-      {/* Footer Container */}
-      <div className="flex flex-col gap-4 items-center max-w-[60rem] my-0 mx-auto  py-6 lg:flex-row lg:justify-between">
-        {/* footer text container */}
-        <div className="text-[#ffffff] text-[1.08rem] leading-[0.9rem] font-bold">
-          <p>Copyright © 2023. All rights are reserved</p>
-        </div>
-        {/* Footer Icon Container */}
-        <div className="flex gap-4 items-center">
-          <div className="text-[#fff] text-[1.5rem]">
-            <a
-              href="https://github.com/eruditmishra"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillGithub />
-            </a>
-          </div>
-          <div className="text-[#fff] text-[1.5rem]">
-            <a
-              href="https://www.linkedin.com/in/uditmishra"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillLinkedin />
-            </a>
-          </div>
-        </div>
+    // Footer Containers
+    <div className="flex bg-[#2d2e32] w-full  gap-4 items-center  justify-center   py-6 lg:flex-row ">
+      {/* footer text container */}
+      <div className="text-[#ffffff] text-[1.08rem] leading-[0.9rem] font-bold">
+        <p>Copyright © 2023. All rights are reserved</p>
       </div>
+      {/* Footer Icon Container */}
+      {/* <div className="flex gap-4 items-center">
+        <div className="text-[#fff] text-[1.5rem]">
+          <a
+            href="https://github.com/eruditmishra"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub />
+          </a>
+        </div>
+        <div className="text-[#fff] text-[1.5rem]">
+          <a
+            href="https://www.linkedin.com/in/uditmishra"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin />
+          </a>
+        </div>
+      </div> */}
     </div>
   );
 };
